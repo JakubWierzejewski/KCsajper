36aefef24e2ba242b64674ff390991864f6c6d85
JakubWierzejewski <118428306+JakubWierzejewski@users.noreply.github.com>
2023-10-16 14:06:23 +0200

    Add files via upload

---
+++ 16_10_2023/index.mjs
@@ -0,0 +1,20 @@
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const app = express()
+const port = 3000
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/index.html'))
+})
+
+app.get('/kontakt',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/contact.html'))
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
